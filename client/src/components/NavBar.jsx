489a624044b7a6c2724cb8aad36fc1dfbb92a2e2
hembee <hembee999@gmail.com>
2022-10-26 06:53:47 +0100

    Navbar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+        <img src="./src/images/airbnb-logo.png" alt="AirBnb Logo" />
+    </nav>
+  )
+}
+
+export default NavBar
