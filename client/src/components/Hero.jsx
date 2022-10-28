5dcc5520743c4cb102ca217ce9307888d38d70c9
hembee <hembee999@gmail.com>
2022-10-28 13:32:26 +0100

    Update Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,8 +3,12 @@ import React from "react";
 const Hero = () => {
   return (
     <section className="hero">
-          <img src={require("../images/grid.png")} alt="" className="hero--img" />
-          
+      <img src={require("../images/grid.png")} alt="" className="hero--img" />
+      <h1 className="hero--header">Online Experiences</h1>
+      <p className="hero--text">
+        Join unique interactive activities led by
+        <br /> one-of-a-kind hosts-all without leaving <br /> home.
+      </p>
     </section>
   );
 };
